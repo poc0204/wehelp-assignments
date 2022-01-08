5902e963a00489b139bcfbdba4d73a4538343388
poc0204 <93992949+poc0204@users.noreply.github.com>
2022-01-08 23:29:51 +0800

    Create week2.js

---
+++ week-2/week2.js
@@ -0,0 +1,107 @@
+function calculate(min,max){
+        const reducer = (previousValue, currentValue) => previousValue + currentValue;
+        var sum1 = max - min
+        
+        var test = 0
+        var sllsum= new Array()
+        for(var i = 0 ; i<=sum1;i++){
+                
+                test = min + i ;
+                sllsum.push(test)
+               
+        }
+        alert(sllsum.reduce(reducer))
+}
+
+calculate(1,3);
+calculate(4,8);
+
+
+function avg(data){
+        
+        var count = data.count
+
+        allsalary = 0
+        for(var i =0; i <= count-1;i++){
+                var salary = data.employees[i].salary
+                allsalary = allsalary + salary
+        }
+        allsalary = allsalary / count
+        alert(allsalary)
+}
+avg({
+        "count":3,
+        "employees":[
+            {
+                "name":"John",
+                "salary":30000
+            },
+            {
+                "name":"Bob",
+                "salary":60000
+            },
+            {
+                "name":"Jenny",
+                "salary":50000
+            },
+        ]
+})
+
+function maxProduct(nums){
+        
+        var text = new Array()
+
+        for(var i = 0;i <=nums.length-1;i++){
+                for(var j = 0;j <=nums.length-1;j++){
+                        text.push(nums[i]*nums[j])
+                }
+        }
+
+        var one = 0 
+        var alltime = nums.length
+     
+        for(var i = 0;i <=nums.length-1;i++){
+                delete text[one];
+                one = one + alltime+1;
+               
+        }
+        var myArrayNew = text.filter(el => el);
+
+        alert( Math.max(...myArrayNew) )
+    
+}
+
+maxProduct([5,20,2,6])
+maxProduct([10,-20,0,3])
+maxProduct([-1,2])
+maxProduct([-1,0,2])
+maxProduct([-1,-2,0])
+
+
+function twoSum(nums, target){
+        var test = 0
+        var nums1 = nums
+        nums.forEach(function(numsitem, numsindex, array) {
+                nums1.forEach(function(nums1item, nums1index, array) {
+                
+                if (numsindex !=nums1index){
+                        test = numsitem+nums1item
+                        
+                }
+                if(test==target){
+                
+                        console.log( numsindex)
+                }
+                
+
+        });
+        });
+                        
+                       
+
+        
+        }
+
+
+let result = twoSum([2,11,7,15],9)
+console.log(result)
