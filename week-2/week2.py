2b3669f05cc83c5f9ebf790198a5d2a7d00a56ba
poc0204 <93992949+poc0204@users.noreply.github.com>
2022-01-11 11:01:42 +0800

    Update week2.py

--- week-2/week2.py
+++ week-2/week2.py
@@ -82,3 +82,24 @@ def towSum(nums,target):
 result=towSum([2,11,7,15],9)
 
 print(result)
+
+def maxZeros(nums):
+    test = 0
+    allsum = []
+    for i in range(len(nums)):
+        
+        if nums[i] == 0 :
+            test = test + 1  
+            if i == len(nums)-1:
+                allsum.append(test)
+
+        if nums[i] == 1 :
+            allsum.append(test)
+            test = 0
+
+    print(max(allsum))
+
+maxZeros([0,1,0,0])
+maxZeros([1,0,0,0,0,1,0,1,0,0])
+maxZeros([1,1,1,1,1])
+maxZeros([0,0,0,1,1])
