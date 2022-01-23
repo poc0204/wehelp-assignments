245804c425c1721a162fa3b65c998cc9ad22d3e3
poc0204 <93992949+poc0204@users.noreply.github.com>
2022-01-23 22:16:59 +0800

    Update week3.js

--- week-3/week3.js
+++ week-3/week3.js
@@ -1,63 +1,75 @@
 
 function reqListener () {
     
-    var ob=JSON.parse(this.responseText)
+    let data=JSON.parse(this.responseText)
     
-    var jpg1 = new RegExp('jpg','i');// 忽略大小寫
-    var http1  = new RegExp('http');
-    var qq = ""
-    var allimgs = new Array()
-    var allstitles = new Array()
-        for(var i = 0 ; i<=ob['result']['results'].length-1;i++){
-        var allimg = ob['result']['results'][i]['file']
-        var  allstitle = ob['result']['results'][i]['stitle'];
-        for(var j = allimg.match(http1)['index'] ;j<= allimg.match(jpg1)['index']+2;j++){
-            qq = qq + allimg[j]
-             
-
-        }
-        allimgs.push(qq)
-        allstitles.push(allstitle)
-        qq = ""
+    let all_jpg = new RegExp('jpg','i');// 忽略大小寫
+    let all_http  = new RegExp('http');
+    let http_jpg = ""
+    var all_imgs = new Array()
+    let all_stitles = new Array()
+        for(var i = 0 ; i<=data['result']['results'].length-1;i++){
+            let allimg = data['result']['results'][i]['file']
+            let  allstitle = data['result']['results'][i]['stitle'];
+            for(var j = allimg.match(all_http)['index'] ;j<= allimg.match(all_jpg)['index']+2;j++){
+                http_jpg = http_jpg + allimg[j]
+            }
+        all_imgs.push(http_jpg)
+        all_stitles.push(allstitle)
+        http_jpg = ""
         allstitle = ""
         
         }
-        for(var i = 0 ; i<=7;i++){
-            var bigImg = document.createElement("img");    
-            bigImg.src=allimgs[i];
-            var myp = document.getElementById('allimgss'+[i]); //获得dom对象
-            myp.appendChild(bigImg);   
-        }
-        for(var i = 0 ; i<=7;i++){
-            var bigp = document.createElement("p");    
-            bigp =  document.createTextNode(allstitles[i]);
-            var pp = document.getElementById('allstitles'+[i]); //获得dom对象
-            pp.appendChild(bigp);
-            
-        }
-        let btn = document.getElementById("btn")
-        let third = document.getElementById("third")
-        var timesstart = 7
-        var timesend = 15
-        btn.addEventListener("click", function(){
-            
-            for(var i = timesstart+1 ; i <= timesend ;i++){
-                if( i < allimgs.length){
-                    third.innerHTML =  third.innerHTML +`
-                
-                    <div class="allimg">  
-                        <p id="allimgss7"><img src="${allimgs[i]}"></p>
-                        <span id="allstitles7"><p>${allstitles[i]}</p></span>
-                    </div>
-                    `;
 
-                }
-                if( i == allimgs.length){
-                    document.getElementById("btn").style.visibility="hidden"
-                }
+        show_img(0,7);
+
+        function show_img(start_time,end_time){
+            let third = document.getElementById("third");
+            for(var i = start_time ; i<=end_time;i++){
+                let all_div = document.createElement("div");
+                all_div.class = "allimg";
+                all_div.id = "allimg"+[i];
+                third.appendChild(all_div);
             }
-            timesstart = timesstart+8
-            timesend =  timesend+8
+            for(var i = start_time ; i<=end_time;i++){
+                let get_img = document.getElementById("allimg"+[i])
+                let put_p = document.createElement("p");     
+                put_p.id = "allimgs"+[i];
+                get_img.appendChild(put_p);
+                let get_p = document.getElementById('allimgs'+[i]);
+                let show_img = document.createElement("img"); 
+                show_img.src=all_imgs[i];
+                get_p.appendChild(show_img);
+            }
+            for(var i = start_time ; i<=end_time;i++){
+                let all_span = document.createElement("span");
+                all_span.id = "all_stitle"+[i];
+                let get_img = document.getElementById("allimg"+[i]);
+                get_img.appendChild(all_span);
+                let get_span = document.getElementById('all_stitle'+[i]);
+                let input_stitle = document.createElement("p");    
+                input_stitle = document.createTextNode(all_stitles[i]);
+                get_span.appendChild(input_stitle);
+            }
+            start_time = start_time + 8; 
+            end_time = end_time + 8;
+            return all_stitles.length
+        }
+        
+        let btn = document.getElementById("btn")
+        let start_time = 8
+        let end_time = 15
+        let click_time = 1
+       
+        btn.addEventListener("click", function(){
+          
+            show_img(start_time,end_time);
+            end_time = end_time + 8
+            start_time = start_time + 8
+            click_time = click_time + 1
+            if(click_time==6){
+                document.getElementById("btn").style.visibility="hidden"
+            }
         })
     }
 
