8dbeaead7567a91942929643be38332ef215f6e3
poc0204 <93992949+poc0204@users.noreply.github.com>
2022-02-11 22:56:00 +0800

    Create app.py

---
+++ week-6/app.py
@@ -0,0 +1,128 @@
+import mysql.connector
+from mysql.connector import Error
+from flask import Flask ,redirect,url_for ,render_template , request , session 
+import re
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "usertest"
+
+@app.route("/")
+def index():
+    username = session.get('username')
+    if username != None:
+        return render_template("member.html")
+    else:
+        return render_template("index.html")
+@app.route("/signup",methods=["POST"])
+def signup():
+    name = request.form["name"]
+    password = request.form["password"]
+    username = request.form["username"]
+    password = request.form["password"]
+    print(username)
+    try:
+        connection = mysql.connector.connect(
+            host = 'localhost',
+            database = 'website',
+            user = 'root',
+            password = 'mysql'
+        )
+        cursor = connection.cursor()
+        sql = " select username from member ; "
+        cursor.execute(sql)
+        all_username = cursor.fetchall()
+        for i in range(len(all_username)):
+            if username == data_clean(all_username[i],1) and password == data_clean(all_username[i],1) :
+                error_message = "帳號已經被註冊"
+                return redirect(url_for("error",message=error_message))
+
+        sql = " insert into member (name ,username ,password) values ('{}' ,'{}' ,'{}');".format(name,username,password)
+        cursor.execute(sql)
+        connection.commit()
+        cursor.close()
+        connection.close()
+        return redirect("/")
+
+    except Error as e:
+        print('database error :',e)
+
+@app.route("/signin",methods=["POST"])
+def signin():
+    username = request.form["username"]
+    password = request.form["password"]
+    try:
+        connection = mysql.connector.connect(
+            host = 'localhost',
+            database = 'website',
+            user = 'root',
+            password = 'mysql'
+        )
+
+
+        cursor = connection.cursor()
+        sql = " select username from member ; "
+        cursor.execute(sql)
+        all_username = cursor.fetchall()
+        sql = " select password from member ; "
+        cursor.execute(sql)
+        all_password = cursor.fetchall()
+        for i in range(len(all_username)):
+            if username == data_clean(all_username[i],1) and password == data_clean(all_password[i],1) :
+                session['username'] = username
+                return redirect("member")
+
+        cursor.close()
+        connection.close()
+        error_message = "帳號、密碼輸入錯誤"
+        return redirect(url_for("error",message=error_message))
+    
+    except Error as e:
+        print('database error :',e)
+
+@app.route("/member")
+def member():
+    username = session.get('username')
+    if username != None:
+        try:
+            connection = mysql.connector.connect(
+                host = 'localhost',
+                database = 'website',
+                user = 'root',
+                password = 'mysql'
+            )
+            cursor = connection.cursor()
+            sql = " select name from member where username= '{}'".format(username)
+            cursor.execute(sql)
+            name = cursor.fetchone()
+            cursor.close()
+            connection.close()
+            return render_template("member.html",name=data_clean(name,1))
+        
+        except Error as e:
+            print('database error :',e)
+    else:
+        return redirect("/")
+
+@app.route("/error/")
+def error():
+    message = request.args.get("message") 
+    return render_template("error.html",error_message={}).format(message)
+
+
+@app.route("/singout",methods=["GET"])
+def singout():
+    session.clear()
+    return  redirect(url_for('index'))
+
+def data_clean(data,method):
+    if method ==0 :
+        data=data.split(",")
+        return data
+    else:
+        string =""
+        for i in data:
+            string=string+i+','
+        return string[:-1]
+
+if __name__=="__main__":
+    app.run(debug=True,port=3000,threaded=True)
