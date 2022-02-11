60174ad3ad2d44a700c5bbbacd54289bdf1da1f3
poc0204 <93992949+poc0204@users.noreply.github.com>
2022-02-11 23:48:12 +0800

    Update app.py

--- week-6/app.py
+++ week-6/app.py
@@ -13,6 +13,7 @@ def index():
         return render_template("member.html")
     else:
         return render_template("index.html")
+        
 @app.route("/signup",methods=["POST"])
 def signup():
     name = request.form["name"]
@@ -20,88 +21,61 @@ def signup():
     username = request.form["username"]
     password = request.form["password"]
     print(username)
-    try:
-        connection = mysql.connector.connect(
-            host = 'localhost',
-            database = 'website',
-            user = 'root',
-            password = 'mysql'
-        )
-        cursor = connection.cursor()
-        sql = " select username from member ; "
-        cursor.execute(sql)
-        all_username = cursor.fetchall()
-        for i in range(len(all_username)):
-            if username == data_clean(all_username[i],1) and password == data_clean(all_username[i],1) :
-                error_message = "帳號已經被註冊"
-                return redirect(url_for("error",message=error_message))
+    connection = link_mysql()
+    cursor = connection.cursor()
+    sql = " select username from member ; "
+    cursor.execute(sql)
+    all_username = cursor.fetchall()
+    for i in range(len(all_username)):
+        if username == data_clean(all_username[i],1) and password == data_clean(all_username[i],1) :
+            error_message = "帳號已經被註冊"
+            return redirect(url_for("error",message=error_message))
 
-        sql = " insert into member (name ,username ,password) values ('{}' ,'{}' ,'{}');".format(name,username,password)
-        cursor.execute(sql)
-        connection.commit()
-        cursor.close()
-        connection.close()
-        return redirect("/")
-
-    except Error as e:
-        print('database error :',e)
+    sql = " insert into member (name ,username ,password) values ('{}' ,'{}' ,'{}');".format(name,username,password)
+    cursor.execute(sql)
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return redirect("/")
 
 @app.route("/signin",methods=["POST"])
 def signin():
     username = request.form["username"]
     password = request.form["password"]
-    try:
-        connection = mysql.connector.connect(
-            host = 'localhost',
-            database = 'website',
-            user = 'root',
-            password = 'mysql'
-        )
 
+    connection = link_mysql()
+    cursor = connection.cursor()
+    sql = " select username from member ; "
+    cursor.execute(sql)
+    all_username = cursor.fetchall()
+    sql = " select password from member ; "
+    cursor.execute(sql)
+    all_password = cursor.fetchall()
+    for i in range(len(all_username)):
+        if username == data_clean(all_username[i],1) and password == data_clean(all_password[i],1) :
+            session['username'] = username
+            return redirect("member")
 
-        cursor = connection.cursor()
-        sql = " select username from member ; "
-        cursor.execute(sql)
-        all_username = cursor.fetchall()
-        sql = " select password from member ; "
-        cursor.execute(sql)
-        all_password = cursor.fetchall()
-        for i in range(len(all_username)):
-            if username == data_clean(all_username[i],1) and password == data_clean(all_password[i],1) :
-                session['username'] = username
-                return redirect("member")
+    cursor.close()
+    connection.close()
+    error_message = "帳號、密碼輸入錯誤"
+    return redirect(url_for("error",message=error_message))
 
-        cursor.close()
-        connection.close()
-        error_message = "帳號、密碼輸入錯誤"
-        return redirect(url_for("error",message=error_message))
-    
-    except Error as e:
-        print('database error :',e)
 
 @app.route("/member")
 def member():
     username = session.get('username')
     if username != None:
-        try:
-            connection = mysql.connector.connect(
-                host = 'localhost',
-                database = 'website',
-                user = 'root',
-                password = 'mysql'
-            )
-            cursor = connection.cursor()
-            sql = " select name from member where username= '{}'".format(username)
-            cursor.execute(sql)
-            name = cursor.fetchone()
-            cursor.close()
-            connection.close()
-            return render_template("member.html",name=data_clean(name,1))
+        connection = link_mysql()
+        cursor = connection.cursor()
+        sql = " select name from member where username= '{}'".format(username)
+        cursor.execute(sql)
+        name = cursor.fetchone()
+        cursor.close()
+        connection.close()
+        return render_template("member.html",name=data_clean(name,1))
         
-        except Error as e:
-            print('database error :',e)
-    else:
-        return redirect("/")
+
 
 @app.route("/error/")
 def error():
@@ -124,5 +98,17 @@ def data_clean(data,method):
             string=string+i+','
         return string[:-1]
 
+def link_mysql():
+    try:
+        connection = mysql.connector.connect(
+            host = 'localhost',
+            database = 'website',
+            user = 'root',
+            password = 'mysql'
+        )
+        
+    except Error as e:
+        print('database error :',e)
+    return connection
 if __name__=="__main__":
     app.run(debug=True,port=3000,threaded=True)
